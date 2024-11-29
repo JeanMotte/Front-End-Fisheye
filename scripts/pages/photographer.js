a9132d5637e96f5e9149d5f07105634a34d56cb4
Jean Motte <jijimotte@gmail.com>
2024-11-29 16:26:59 +0100

    Add photograph header

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,24 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+import Api from '../api/api_constructor.js'
+import Header from '../templates/header.js'
+
+const api = new Api('../../data/photographers.json')
+const photographerId = new URLSearchParams(window.location.search).get('id')
+
+async function init() {
+  const { photographers, media } = await api.get()
+  const photographerData = photographers.find(
+    (photographer) => photographer.id === parseInt(photographerId)
+  )
+  const photographerMedias = media.filter(
+    (media) => media.photographerId === parseInt(photographerId)
+  )
+  console.log(photographerData, photographerMedias)
+
+  // return { photographerData, photographerMedias }
+  const headerTemplate = new Header(photographerData)
+  headerTemplate.setHeader()
+}
+
+init()
---
+++ scripts/templates/header.js
@@ -0,0 +1,29 @@
+export default class Header {
+  constructor(photographer) {
+    this.photographer = photographer
+  }
+
+  setHeader() {
+    const headerSpace = document.querySelector('.photograph-header')
+
+    const info = `
+      <div class="photograph-header__info">
+        <h1 class="name">${this.photographer.name}</h1>
+        <div class="location-tagline-target">
+          <p class="location">${this.photographer.city}, ${this.photographer.country}</p>
+          <p class="tagline">${this.photographer.tagline}</p>
+        </div>
+      </div>
+      <div>
+        <button class="contact_button" onclick="displayModal()">
+            Contactez-moi
+        </button>
+      </div>
+      <div class="photograph-header__portrait" id="tags">
+        <img src="/assets/photographers/${this.photographer.portrait}" alt="Photo de ${this.photographer.name}" class="" />
+      </div>
+    `
+
+    headerSpace.innerHTML = info
+  }
+}
