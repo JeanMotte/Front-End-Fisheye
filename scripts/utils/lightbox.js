267dec8d66445afa245139acdea1e4103c3128ac
Jean Motte <jijimotte@gmail.com>
2024-12-13 16:23:44 +0100

    update

--- scripts/utils/lightbox.js
+++
@@ -1,8 +0,0 @@
-const closeBtn = document.querySelector('.lightbox__close')
-const lightbox = document.querySelector('.lightbox')
-
-if (closeBtn && lightbox) {
-  closeBtn.addEventListener('click', () => {
-    lightbox.style.display = 'none'
-  })
-}
